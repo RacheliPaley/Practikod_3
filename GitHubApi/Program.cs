abe3eafafd62bf13598ee1f5f6225a96d1fd8972
רחל פאלי ExtraSchoolW <esw16more21@gmail.com>
2023-07-05 00:03:45 +0300

    my project

---
+++ GitHubApi/Program.cs
@@ -0,0 +1,29 @@
+using Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//var folderPath = builder.Configuration["folderPath"];
+builder.Services.Configure<GitHubIntegrationOptions>(builder.Configuration.GetSection(nameof(GitHubIntegrationOptions)));
+builder.Services.AddGitHubIntegrations(options=>builder.Configuration.GetSection(nameof(GitHubIntegrationOptions)).Bind(options));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
